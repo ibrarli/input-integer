7c96037a31bc5b76c81c0aa6ad619016a1d198cb
Ibrar-Haidar <106898965+Ibrar-Haidar@users.noreply.github.com>
2025-06-06 11:46:40 +0500

    update demo

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,5 +1,22 @@
-const inputInteger = require('..')
+const inputInteger = require("..");
 
-const x = inputInteger()
+const input1 = inputInteger();
+const input2 = inputInteger();
 
-document.body.innerHTML = `<h1> ${x} </h1>`
+
+const title = "my demo form";
+const subtitle = "please fill out the form";
+
+const page = document.createElement("div");
+page.innerHTML = `
+    <h1> ${title} </h1>
+    <h2> ${subtitle} </h2>
+    <h3> Enter your age </h3>
+    <x></x>
+    <h3> Enter your age </h3>
+    <y></y>
+    `;
+document.body.append(page);
+page.querySelector("x").replaceWith(input1);
+page.querySelector("y").replaceWith(input2);
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-module.exports = inputInteger
+module.exports = inputInteger;
 
-function inputInteger(){
-    const name = 'input-integer'
-    return name 
+const sheet = new CSSStyleSheet();
+const theme = get_theme();
+sheet.replaceSync(theme);
+
+function inputInteger() {
+  const el = document.createElement("div");
+  const shadow = el.attachShadow({ mode: "closed" });
+  const input = document.createElement("input");
+  input.type = "number";
+  input.min = 0;
+  input.max = 10;
+  input.onkeyup = (e) => handle_onkeyup(e, input);
+
+  shadow.append(input);
+
+  shadow.adoptedStyleSheets = [sheet];
+  return el;
+}
+
+function get_theme() {
+  return `
+        input {
+            padding: 4px 10px;
+            background-color: hsla(284, 45%, 54%, 1);
+            border-radius: 20px;
+        }
+        input:focus {
+        background-color: red;
+        }
+    `;
+}
+
+function handle_onkeyup(e, input) {
+  console.log(e.target.value);
+  const val = Number(e.target.value);
+
+  if (input.max < val) {
+    input.value = 150;
+  }
+  else if (input.min > val){
+    input.value = 0;
+  }
 }
